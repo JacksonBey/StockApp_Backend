1c31929b654f5850ce503de880d0365946bc146c
jasperwreed <jmixon.reed@gmail.com>
2020-11-08 15:58:00 -0800

    worked on login and logout, may need to be refactored

--- Gemfile
+++ Gemfile
@@ -42,3 +42,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'fast_jsonapi'
 
+gem 'rest-client'
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/login'
+  get 'sessions/welcome'
   resources :watch_lists
   resources :stocks
   resources :users
